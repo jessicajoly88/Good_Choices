99fe30a711b10eaf113a97dbc21d0d668ff428bb
Jessica Joly & Yelena Nedelko <student@epicodus.com>
2015-10-26 14:54:30 -0700

    Implement AJAX for new question.

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -3,6 +3,9 @@ class QuestionsController < ApplicationController
     @questions = Question.all
   end
 
+  def show
+  end
+
   def new
     @question = Question.new
   end
@@ -10,7 +13,10 @@ class QuestionsController < ApplicationController
   def create
     @question = Question.new(question_params)
     if @question.save
-      redirect_to root
+      respond_to do |format|
+        format.html { redirect_to questions_path }
+        format.js
+      end
     else
       render :new
     end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
   root to: "questions#index"
 
-  resources :questions do
-    resources :options
-  end
+  resources :questions 
 
 end
